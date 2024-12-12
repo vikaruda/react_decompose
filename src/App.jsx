8d05ff73383b650bd45773b4e18c604dc6b428df
Vika Ruda <rudaviktoria0@gmail.com>
2024-12-12 08:42:30 +0200

    Solution1212

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import './App.css';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 import Welcome from './components/Welcome/Welcome';
-import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <main className="app">
       <Welcome />
       <Header />
-      <Navigation />
       <Article />
     </main>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,13 @@
 
 // Add a default export statement for Header component to use it in the other files
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+      <Navigation />
     </header>
   );
 }
